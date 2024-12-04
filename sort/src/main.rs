326e817e6166691b8f67f1f4e84c88f77892ef2f
ShirasakiAtsuro <atsuro.shirasaki@lemon-tart.jp>
2024-12-04 12:48:30 +0900

    自分でsortを定義しました

--- sort/src/main.rs
+++ sort/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
     let mut number = vec![1, 5, 2, 4, 3];
 
-    number.sort();
+    sort(&mut number);
 
     println!("{:?}", number);
+}
+
+fn sort(vec: &mut Vec<i32>) {
+    for i in 0..vec.len() {
+        let mut tmp = i;
+        for j in i..vec.len() {
+            if vec[tmp] < vec[j] {
+                tmp = j;
+            }
+        }
+
+        let tp = vec[i];
+        vec[i] = vec[tmp];
+        vec[tmp] = tp;
+    }
 }
---
+++ sort/target/debug/sort.d
@@ -0,0 +1,1 @@
+/Users/atsuroshirasaki/Rust/pull-request-tes/sort/target/debug/sort: /Users/atsuroshirasaki/Rust/pull-request-tes/sort/src/main.rs
